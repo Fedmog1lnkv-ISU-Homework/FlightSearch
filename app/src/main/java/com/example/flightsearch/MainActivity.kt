d85ad4c824a0095d62245f7f10fdeca7fef42fb4
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-11-10 00:31:27 +0800

    feat: Created main activity

--- app/src/main/java/com/example/flightsearch/MainActivity.kt
+++ app/src/main/java/com/example/flightsearch/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.flightsearch
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import com.example.flightsearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    fun initViews() = binding.apply {
+        spinnerArrival.adapter =
+            ArrayAdapter<String>(
+                this@MainActivity,
+                android.R.layout.simple_spinner_item,
+                resources.getStringArray(R.array.cities)
+            )
+        spinnerDeparture.adapter =
+            ArrayAdapter<String>(
+                this@MainActivity,
+                android.R.layout.simple_spinner_item,
+                resources.getStringArray(R.array.cities)
+            )
+        editTextArrivalDate.setupDatePicker()
+        editTextDepartureDate.setupDatePicker()
+    }
+
+    fun EditText.setupDatePicker() {
+        setOnClickListener {
+            DatePickerDialog(this@MainActivity).apply {
+                setOnDateSetListener { _, year, month, dayOfMonth ->
+                    val string = "$dayOfMonth.${month + 1}.$year"
+                    setText(string)
+                }
+                show()
+            }
+        }
     }
 }
